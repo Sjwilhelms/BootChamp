491a07d141761bd683ec586529843590ae4c1a11
Sam Williams <sjwilliams388@gmail.com>
2024-11-27 10:15:14 +0000

    add basic views

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('profile/', include("forum.urls"), name="profile-urls"),
     path('', include('forum.urls'), name='home'),
 ]
