045241b29f4e121845d12ce19bbc127b019de483
sjwilhelms <sjwilliams388@gmail.com>
2024-12-10 19:38:19 +0000

    test deploy mvp

---
+++ staticfiles/js/toasts.js
@@ -0,0 +1,10 @@
+// Automatically show toasts when the page loads
+document.addEventListener('DOMContentLoaded', function () {
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'));
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl, {
+            autohide: true,
+            delay: 5000  // 5 seconds
+        }).show();
+    });
+});
